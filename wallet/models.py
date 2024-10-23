7d293573deafb26409598c2fb1f42ada121e198c
Hryhoryk Paul <pavelminsk4@gmail.com>
2024-10-23 13:50:36 +0200

    Add CustomPagination

--- wallet/models.py
+++ wallet/models.py
@@ -9,7 +9,7 @@ class Wallet(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class Transactrion(models.Model):
+class Transaction(models.Model):
     wallet     = models.ForeignKey(Wallet, on_delete=models.CASCADE)
     tx_id      = models.CharField(max_length=64, unique=True)
     amount     = models.DecimalField(max_digits=18, decimal_places=2)
--- wallet/views.py
+++ wallet/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Wallet, Transaction
+from .serializers import WalletSerializer, TransactionSerializer
+from rest_framework_json_api.pagination import JsonApiPageNumberPagination
 
-# Create your views here.
+
+class CustomPagination(JsonApiPageNumberPagination):
+    page_query_param = 'page_number'
+    page_size_query_param = 'page_length'
+    page_size = 2
+    max_page_size = 100
+
+
+class WalletViewSet(viewsets.ModelViewSet):
+    queryset = Wallet.objects.all()
+    serializer_class =  WalletSerializer
+    pagination_class = CustomPagination
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    pagination_class = CustomPagination
